dec55358a003105667e1c29020e0dd0e6131778e
JonAlmonte <60440978+JonAlmonte@users.noreply.github.com>
2020-02-26 12:36:25 -0500

    Add files via upload

---
+++ Printer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Jonnathan
+ * 
+ */
+package Classwork;
+
+public class Printer {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the number printer.");
+		
+		int n= 100;
+		
+		for(int i=5;i<n;i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+}
